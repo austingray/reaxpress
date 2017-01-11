9e96de74605f27ffbe7f5aa9d740dfd3278238d9
Austin Gray <waustingray@gmail.com>
2017-01-11 04:02:28 +0000

    added route command

--- bin/reaxpress-cli.js
+++ bin/reaxpress-cli.js
@@ -1,2 +1,29 @@
 #!/usr/bin/env node
-console.log('Hello, world!');
+const program = require('commander');
+
+const commands = {};
+commands.route = function(arg) {
+  console.log('command:', cmdValue);
+  console.log('argument:', argValue);
+}
+
+program
+  .version('0.0.1')
+  .action(function(cmd, arg) {
+    cmdValue = cmd;
+    argValue = arg
+  });
+
+program.parse(process.argv);
+
+if (typeof cmdValue === 'undefined') {
+  console.error('no command given!');
+  process.exit(1);
+}
+
+if (typeof commands[cmdValue] === 'undefined') {
+  console.error(`'${cmdValue}' is not a valid command.`);
+  process.exit(1);
+}
+
+commands[cmdValue](argValue);
