b9ba95747493a443fe0fa94b800bd446d5cb51eb
Austin Gray <waustingray@gmail.com>
2017-01-11 06:56:13 +0000

    added route command to cli tool

--- README.md
+++ README.md
@@ -13,6 +13,29 @@ What this is not:
 
  - This is not a SPA (may change in the future).
 
+### Usage
+
+To create an alias for the 'reaxpress' command:
+
+    npm install -g
+    reaxpress <cmd> <arg>
+
+Or call the script directly
+
+    ./bin/reaxpress-cli.js <cmd> <name>
+
+Commands:
+
+    reaxpress route <name>
+
+This will:
+ - create ./routes/<name>.jsx with boilerplate get functionality
+ - create ./src/react/<name>/index.jsx with boilerplate components/copy
+ - modify ./app.js to define and mount your route
+ - modify ./webpack.config.js to add your new component as an entry point
+
+This is a very crude implementation still and needs to check if the route name already exists, as well as do some additional checks.
+
 ### Setup and Installation
 
 This project follows [Airbnb React/JSX Style Guide](https://github.com/airbnb/javascript/tree/master/react). Install a linter (Atom editor):
--- bin/reaxpress-cli.js
+++ bin/reaxpress-cli.js
@@ -1,12 +1,110 @@
 #!/usr/bin/env node
+/* eslint consistent-return: 0, no-shadow: 0 */
 const program = require('commander');
+const fs = require('fs');
+const path = require('path');
+
+const webpackConfigFile = path.join(__dirname, '..', 'webpack.config.js');
+const appFile = path.join(__dirname, '..', 'app.js');
 
 let cmdValue = '';
 let argValue = '';
 
+const mkdirSync = (path) => {
+  try {
+    fs.mkdirSync(path);
+  } catch (e) {
+    if (e.code !== 'EEXIST') throw e;
+  }
+};
+
 const commands = {};
 commands.route = (arg) => {
-  console.log('argument:', arg);
+  console.log(arg);
+  // update app.js
+  fs.readFile(appFile, 'utf8', (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    let modified = data.replace(/#route-def/g, `#route-def\nconst ${arg} = require('./routes/${arg}');`);
+    modified = modified.replace(/#route-mount/g, `#route-mount\napp.use('/${arg}', ${arg});`);
+    fs.writeFile(appFile, modified, 'utf8', (err) => {
+      if (err) return console.log(err);
+      console.log('successfully added route to app file.');
+    });
+  });
+  // add react components if they do not already exist
+  const reactDirPath = path.join(__dirname, '..', 'src/react/', arg);
+  const reactFile = path.join(reactDirPath, 'index.jsx');
+  const reactComponentName = arg.charAt(0).toUpperCase() + arg.slice(1);
+  console.log(reactComponentName);
+  mkdirSync(reactDirPath);
+  const componentCode = `import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from '../global/Header';
+import Footer from '../global/Footer';
+import Page from '../global/Page';
+
+function ${reactComponentName}() {
+  return (
+    <div>
+      <Header />
+      <Page>
+        ${reactComponentName} content
+      </Page>
+      <Footer />
+    </div>
+  );
+}
+
+if (typeof document !== 'undefined') {
+  ReactDOM.render(
+    <${reactComponentName} />,
+    document.getElementById('app'),
+  );
+}
+
+export default ${reactComponentName};
+`;
+  fs.writeFile(reactFile, componentCode, 'utf8', (err) => {
+    if (err) return console.log(err);
+    console.log('successfully created react component file.');
+  });
+  // create route file
+  const routeFile = path.join(__dirname, '..', 'routes', `${arg}.jsx`);
+  const routeCode = `import ${reactComponentName} from '../src/react/${arg}';
+
+const express = require('express');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('template.ejs', {
+    templateHtml: ReactDOMServer.renderToString(<${reactComponentName} />),
+    componentJs: '${arg}',
+  });
+});
+
+module.exports = router;
+`;
+  fs.writeFile(routeFile, routeCode, 'utf8', (err) => {
+    if (err) return console.log(err);
+    console.log('successfully created route file.');
+  });
+  // update webpack.config.js
+  fs.readFile(webpackConfigFile, 'utf8', (err, data) => {
+    if (err) {
+      return console.log(err);
+    }
+    const modified = data.replace(/entry: {/g, `entry: {\n    ${arg}: './src/react/${arg}',`);
+    fs.writeFile(webpackConfigFile, modified, 'utf8', (err) => {
+      if (err) return console.log(err);
+      console.log('successfully added entry to webpack config file.');
+    });
+  });
 };
 
 program
