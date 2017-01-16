951ab969477314564b7d49b30666488ada1c79f6
Austin Gray <waustingray@gmail.com>
2017-01-16 16:49:46 +0000

    included favicon

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const bodyParser = require('body-parser');
 const expressSession = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
-// const favicon = require('serve-favicon');
+const favicon = require('serve-favicon');
 const app = express();
 
 // view engine setup
 app.set('views', __dirname);
 app.set('view engine', 'ejs');
 
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
