976c24313034c22e8586c7062377988d2087a262
Austin Gray <waustingray@gmail.com>
2017-04-16 17:01:11 +0000

    added production config to knexfile

--- .knex/knexfile.js
+++ .knex/knexfile.js
@@ -1,15 +1,16 @@
-module.exports = {
-
-  development: {
-    client: 'pg',
-    connection: process.env.REAXPRESS_CONNECTION_STRING,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
+const config = {
+  client: 'pg',
+  connection: process.env.REAXPRESS_CONNECTION_STRING,
+  pool: {
+    min: 2,
+    max: 10,
   },
+  migrations: {
+    tableName: 'knex_migrations',
+  },
+};
 
+module.exports = {
+  development: config,
+  production: config,
 };
