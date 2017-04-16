89f52dd99e6b7ab4be6f76852ac1ff2b89dcf522
Austin Gray <waustingray@gmail.com>
2017-04-16 16:44:47 +0000

    removed prestart script, added start-dev script

--- bin/prestart.sh
+++
@@ -1,5 +0,0 @@
-node ./node_modules/knex/bin/cli.js --knexfile .knex/knexfile.js migrate:rollback
-node ./node_modules/knex/bin/cli.js --knexfile .knex/knexfile.js migrate:latest --env ${NODE_ENV}
-node ./node_modules/knex/bin/cli.js --knexfile .knex/knexfile.js seed:run
-node-sass --include-path scss src/scss/style.scss public/build/css/style.css
-webpack --progress --colors --display-error-details
