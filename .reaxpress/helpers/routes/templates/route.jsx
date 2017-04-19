7daf147c794f505f301d9302628ffd94e58fcbf9
Austin Gray <waustingray@gmail.com>
2017-04-19 04:47:01 +0000

    removed reassignment back to res.locals

--- .reaxpress/helpers/routes/templates/route.jsx
+++ .reaxpress/helpers/routes/templates/route.jsx
@@ -11,8 +11,9 @@ const router = express.Router();
 router.get('/', (req, res) => {
   // res.locals.reaxpressData passes the app state on the server
   const reaxpressData = res.locals.reaxpressData;
+
   // customize the reaxpress app state here, for example:
-  reaxpressData.custom = { key: 'value' };
+  // reaxpressData.custom = { key: 'value' };
 
   // if the url has ?reaxpress=true appended,
   // it means it's a fresh request to an endpoint.
@@ -21,11 +22,8 @@ router.get('/', (req, res) => {
     return res.json(reaxpressData);
   }
 
-  // otherwise, this is the entry point of the app,
-  // send a server render and mount it on the front end.
-  // res.locals.reaxpressData is embedded in the server response
-  res.locals.reaxpressData = JSON.stringify(reaxpressData);
-  return res.send(template(reaxpressData, renderToString(<${parsedArgs.component} />)));
+  // otherwise this is the entry point of the app, send a server render
+  return res.send(template(reaxpressData, renderToString(<${parsedArgs.component} reaxpressData={reaxpressData} />)));
 });
 
 // end of #reaxpress routes
