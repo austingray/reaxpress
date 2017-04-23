ef0ddf91c66646e500e9fe0a25aac9025a1243f4
Austin Gray <waustingray@gmail.com>
2017-04-23 23:25:26 +0000

    added getRouteFilePath method

--- .reaxpress/helpers/routes/index.jsx
+++ .reaxpress/helpers/routes/index.jsx
@@ -1,3 +1,4 @@
+import path from 'path';
 import create from './create';
 import removeParent from './removeParent';
 import removeChild from './removeChild';
@@ -6,4 +7,15 @@ export default {
   create,
   removeParent,
   removeChild,
+  getRouteFilePath(args) {
+    // reject bad arguments
+    if (typeof args.parent !== 'string' || args.parent === '') {
+      console.log('Error getting route file path:');
+      console.log(`type: ${typeof args.parent}`);
+      console.log(`value: ${args.parent}`);
+      throw new Error(`Error in ${path.join(__dirname, 'index.jsx')}`);
+    }
+    // route file is always named after the parent
+    return path.join(__dirname, '../../..', 'routes', `${args.parent}.jsx`);
+  }
 };
